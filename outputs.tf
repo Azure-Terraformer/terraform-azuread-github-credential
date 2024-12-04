a19ef218a14e0ac245bd504492c43443c21d8914
= <=>
2024-12-04 15:45:36 -0500

    outputs

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,12 @@
-output "application_id" {
-  value = azuread_application.main.client_id
+output "application" {
+  value = {
+    client_id = azuread_application.main.client_id
+    object_id = azuread_application.main.object_id
+  }
 }
-output "service_principal_id" {
-  value = azuread_service_principal.main.client_id
+output "service_principal" {
+  value = {
+    client_id = azuread_service_principal.main.client_id
+    object_id = azuread_service_principal.main.object_id
+  }
 }
